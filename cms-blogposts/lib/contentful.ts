f224766a1c0f7d789fd02052d239182472920c94
makoto2025 <aratamakoto2025@gmail.com>
2026-09-13 20:46:46 -0700

    fix contentful connection

--- cms-blogposts/lib/contentful.ts
+++ cms-blogposts/lib/contentful.ts
@@ -7,7 +7,7 @@ export type BlogPostSkeleton = {
 		slug: EntryFieldTypes.Text;
 		featuredImage?: EntryFieldTypes.AssetLink;
 		date: EntryFieldTypes.Date;
-		tags: EntryFieldTypes.Text;
+		tags: EntryFieldTypes.Text[];
 		content: EntryFieldTypes.RichText;
 	};
 };
@@ -27,16 +27,27 @@ const client = createClient({
 export async function getAllPosts(): Promise<BlogPost[]> {
 	const res = await client.getEntries<BlogPostSkeleton>({
 		content_type: "blogPosts",
+		order: ["-fields.date"],
 	});
 	return res.items;
 }
 
-// GET SINGLE POST
-export async function getPostById(id: string): Promise<BlogPost | null> {
+// // GET SINGLE POST by ID
+// export async function getPostById(id: string): Promise<BlogPost | null> {
+// 	const res = await client.getEntries<BlogPostSkeleton>({
+// 		content_type: "blogPosts",
+// 		"sys.id": id,
+// 		order: ["-sys.createdAt"],
+// 	});
+// 	return res.items[0] ?? null;
+// }
+
+// // GET SINGLE POST by Slug
+export async function getPostBySlug(slug: string): Promise<BlogPost | null> {
 	const res = await client.getEntries<BlogPostSkeleton>({
 		content_type: "blogPosts",
-		"sys.id": id,
-		order: ["-sys.createdAt"],
+		"fields.slug": slug,
+		limit: 1,
 	});
 	return res.items[0] ?? null;
 }
